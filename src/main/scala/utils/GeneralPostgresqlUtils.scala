7b4e586232c470e70abc5e93e4d6bbe74432e697
dariuszlee <dariuszlee93@gmail.com>
2019-04-03 23:19:21 +0200

    1. Adding functionality to Postgresql utils

--- src/main/scala/utils/GeneralPostgresqlUtils.scala
+++ src/main/scala/utils/GeneralPostgresqlUtils.scala
@@ -4,6 +4,9 @@ import akka.actor.Actor
 import akka.actor.Props
 import akka.stream.ActorMaterializer
 
+import java.sql.DriverManager
+import java.sql.Connection
+
 object PostgresUtils {
   def props : Props = Props(new PostgresUtils())
 
@@ -13,8 +16,22 @@ object PostgresUtils {
 class PostgresUtils extends Actor with akka.actor.ActorLogging {
   import PostgresUtils._
 
+  val username = "dariuslee"
+  val url = "jdbc:postgresql:mus_sync_test"
+  val connection : Connection = DriverManager.getConnection(url, username, "ma456tilda")
+
   override def receive = {
-    case InitializeEmpty => {}
+    case InitializeEmpty => {
+      val statement = connection.createStatement()
+
+      val create_user_table = "CREATE TABLE IF NOT EXISTS mus_sync_user(id varchar UNIQUE NOT NULL, login varchar UNIQUE NOT NULL, password_hash varchar UNIQUE NOT NULL)"
+      val create_spotify_account = "CREATE TABLE IF NOT EXISTS spotify_user(spotify_id VARCHAR UNIQUE NOT NULL, id varchar REFERENCES mus_sync_user(id), refresh_token VARCHAR)"
+
+      statement.execute(create_user_table)
+      statement.execute(create_spotify_account)
+      statement.close()
+      log.info("Created tables: create_user_table, create_spotify_account")
+    }
   }
 }
 
