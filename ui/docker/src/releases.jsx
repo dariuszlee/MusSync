52a23b4dd136a873f4569ef3686affc05c64e93b
dariuszlee <dariuszlee93@gmail.com>
2019-06-02 01:19:00 +0200

    Docker tests

---
+++ ui/docker/src/releases.jsx
@@ -0,0 +1,17 @@
+'use strict';
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+      return <button onClick={() => this.setState({ liked: true })}>
+               Like Everything 
+             </button>
+  }
+}
+
+const domContainer = document.querySelector('#release_curr');
+ReactDOM.render(React.createElement(LikeButton), domContainer);
